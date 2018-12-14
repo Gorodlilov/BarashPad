e1dc0db764c6d4b366abf0465d87f4bed14a6156
Kirill GorodiloV <45821562+BarawikS@users.noreply.github.com>
2018-12-14 21:06:53 +1000

    Add files via upload

--- Barapad.lua
+++ Barapad.lua
@@ -23,7 +23,7 @@ local COLOR1 = 0x00ffff --Рамка
 local COLOR2 = 0x0000ff --Цвет кнопок
 local COLOR3 = 0x333333 --Таблица
 local COLOR_SHELL = 0xff00ff --Цвет шелла
-local PASS = "Developp" - Пасс
+local PASS = "Developp" -- Пасс
 -------------------------------------------------
 if not (fs.exists(shell.getWorkingDirectory() .. "/Programms.lua")) then
 	shell.execute("wget https://raw.githubusercontent.com/BarawikS/BarashPad/master/Programms.lua Programms.lua")
