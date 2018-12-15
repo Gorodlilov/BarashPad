8358d275f79131bf49a6619f7465030e25391351
Kirill GorodiloV <45821562+BarawikS@users.noreply.github.com>
2018-12-15 17:23:18 +1000

    Update antimute.lua

--- programms/antimute.lua
+++ programms/antimute.lua
@@ -15,7 +15,7 @@ cb.setDistance(100)
 local term=require("term")
 local g=component.gpu
 local colors = require("colors")
-cb.setName("§6[§bAntiMute§6]")
+cb.setName("§bAntiMute§6")
 term.clear()
 
 local WIGHT, HEIGHT = 146, 42 --Разрешение моника 146/112 x 42
