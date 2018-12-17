ee2919c3a3b7acfe80b90444cb54deeb6eb39a33
Kirill GorodiloV <45821562+BarawikS@users.noreply.github.com>
2018-12-17 22:13:02 +1000

    Update Barapad.lua

--- Barapad.lua
+++ Barapad.lua
@@ -84,8 +84,8 @@ function Login()
 	Bar.MidR(WIGHT,32,"Введите пароль:")
 	term.setCursor(mid-2,33)
 	local p, nick = Bar.Read({mask = "*", max = 8, accept = "0-9a-f", blink = true, center = true, nick = true})
-	if p==[Bar.PASSES] then
-		if nick == [Bar.NICK] then
+	if p==Bar.PASSES then
+		if nick == Bar.NICK then
 			login = true
 			Bar.MidR(WIGHT,33,"Приветствую, " .. nick)
 			computer.addUser(nick)
@@ -116,8 +116,8 @@ function Login()
 	end
 		end
 		
-	if p~=[Bar.PASSES] then
-		if nick ~= [Bar.NICK] then
+	if p~=Bar.PASSES then
+		if nick ~= Bar.NICK then
 			login = true
 			Bar.MidR(WIGHT,33,"Неверный пароль, " .. nick)
 			os.sleep(1)
