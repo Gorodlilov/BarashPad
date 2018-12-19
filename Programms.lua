c32e55e32669a88c25a6c413203926dd5a54a64d
Kirill GorodiloV <45821562+BarawikS@users.noreply.github.com>
2018-12-19 14:52:09 +1000

    Update Programms.lua

--- Programms.lua
+++ Programms.lua
@@ -1,11 +1,3 @@
---{{"OpenChest"," - Кейсики"}, "http://pastebin.com/raw/QTCJZq3c",{"internet","chat_box"}},
---{{"AncientEmerald MOD"," - Халявные эмы"}, "https://pastebin.com/raw/bGPdFqvr",{"internet"}},
---{{"OpenAdmins"," - Мод состав"},"http://pastebin.com/raw/xN5AFT0u",{"internet","opencb"}},
---{{"OpenInfo"," - Инфо панель"},"http://pastebin.com/raw/teGcGxXE",{"internet"}},
-{{"OpenBc"," - Объявления (A: Shyvana)"},"http://pastebin.com/raw/GtPthhpm",{"internet", "chat_box"}},
---{{"OpenRep"," - Сис-ма рейтингов"},"http://pastebin.com/raw/E0ih3kP6",{"internet", "chat_box", "opencb"}},
---{{"Bankomat"," - банк"},"http://pastebin.com/raw/t3yvmr68",{"internet", "opencb"}},
---{{"UChat"," - чат избранных (A: InfinityDark)"},"",{"Недоступен"}},
+{{"OpenBc"," - Объявления (A: Shyvana)"},"https://raw.githubusercontent.com/BarawikS/BarashPad/master/programms/OpenBC.lua",{"internet", "chat_box"}},
 {{"Holograms"," - голограммы"},"https://raw.githubusercontent.com/BarawikS/Holo/master/holograms/holo_print.lua",{"internet", "chat_box"}},
-{{"Antimute"," - обход мута в локальном чате (A: InfinityDark)"},"https://raw.githubusercontent.com/BarawikS/BarashPad/master/programms/antimute.lua",{"chat_box"}}
---{{"Bankomat MOD"," - банк"},"http://pastebin.com/raw/yTfthrA2",{"internet"}}
+{{"Antimute"," - обход мута в локале (A: InfinityDark)"},"https://raw.githubusercontent.com/BarawikS/BarashPad/master/programms/antimute.lua",{"chat_box"}}
