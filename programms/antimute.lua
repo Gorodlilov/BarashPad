54c1c1a8d39088ec0c64ffb00f13e9a7a915d3d2
Kirill GorodiloV <45821562+BarawikS@users.noreply.github.com>
2019-03-16 17:00:14 +1000

    Update antimute.lua

--- programms/antimute.lua
+++ programms/antimute.lua
@@ -18,7 +18,7 @@ cb.setDistance(100)
 local term=require("term")
 local g=component.gpu
 local colors = require("colors")
-cb.setName("§bAntiMute§7")
+cb.setName(..plr)
 term.clear()
 
 local WIGHT, HEIGHT = 146, 42 --Разрешение моника 146/112 x 42
@@ -56,17 +56,24 @@ if msg=="-stop" then
 rem(plr)
 cb.say("§8[§c-§8] §f".. plr)
 end
-	
 
 if msg=="-closeapp" then
 print("\nЗакрытие программы..")
 os.sleep(1)
 os.execute("reboot")
 end
-
+	
+if msg=="-updateapp" then
+cb.say("\nНачинаю обновление программы.")
+os.sleep(1)
+os.execute("wget -f https://raw.githubusercontent.com/BarawikS/BarashPad/master/programms/antimute.lua Antimute.lua")
+os.sleep(1)
+os.execute("Antimute.lua")	
+end
+	
 term.clear()
 for i,k in pairs(plrs) do
    g.set(1,i,k)
-   if k==plr then cb.say(plr.."§f: §b"..msg) end
+   if k==plr then cb.say(§b"..msg) end
 end
 end
