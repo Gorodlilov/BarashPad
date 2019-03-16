f5f9d3e834e2e6159a3c8cba9d808cce32253ad3
Kirill GorodiloV <45821562+BarawikS@users.noreply.github.com>
2019-03-16 15:15:28 +1000

    Update antimute.lua

--- programms/antimute.lua
+++ programms/antimute.lua
@@ -32,7 +32,6 @@ for k,v in pairs(plrs) do
 if v==nick then 
 return true
 end
-		end
 while true do
 evt,_,plr,msg=event.pull("chat_message")
 if msg=="-start" then	
@@ -41,12 +40,13 @@ cb.say("§8[§a+§8] §f" ..plr)
 return false
 end
 end
+		end
 			
 if msg=="-stop" then
 rem(plr)
 cb.say("§8[§c-§8] §f".. plr)
 end
-	end
+end
 local function rem(player)
 for i,k in pairs(plrs) do
 if k==player then
