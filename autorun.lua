de144dc1dcf4d40d398feafef869804f16e8b919
Kirill GorodiloV <45821562+BarawikS@users.noreply.github.com>
2019-04-18 17:57:46 +1000

    Autorun

---
+++ autorun.lua
@@ -0,0 +1,15 @@
+local shell = require('shell')
+local term = require('term')
+os.sleep(0.5)
+term.clear()
+local dir = '" .. shell.getWorkingDirectory() .. "
+if dir ~= '/' then shell.setWorkingDirectory(dir) end
+shell.execute("rm Barapad.lua")
+shell.execute("rm Programms.lua")
+shell.execute("rm /lib/Bar.lua")
+os.sleep(2)
+os.sleep(1)
+shell.execute("wget https://raw.githubusercontent.com/BarawikS/BarashPad/master/Barlib.lua /lib/Bar.lua")
+shell.execute("wget https://raw.githubusercontent.com/BarawikS/BarashPad/master/Barapad.lua Barapad.lua")
+shell.execute("wget https://raw.githubusercontent.com/BarawikS/BarashPad/master/Programms.lua Programms.lua")
+shell.execute('Barapad')
