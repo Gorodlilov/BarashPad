ef1b94fdca80938ccef721cfc0d2a217f281a387
Kirill GorodiloV <45821562+BarawikS@users.noreply.github.com>
2019-08-20 14:51:36 +1000

    LOGGER UPDATE

--- Programms.lua
+++ Programms.lua
@@ -1,6 +1,5 @@
 {{"OpenBc"," - Объявления (A: Shyvana)"},"https://raw.githubusercontent.com/BarawikS/BarashPad/master/programms/OpenBC.lua",{"internet", "chat_box"}},
 {{"Holograms"," - голограммы"},"https://gorodlilov.ru/raw/holoedit.lua",{"internet", "hologram"}},
 {{"Board"," - инф. стенд"},"https://raw.githubusercontent.com/BarawikS/BarashPad/master/programms/board.lua",{"internet"}},
-{{"RabbitBot"," - бот на варпец :3"},"https://raw.githubusercontent.com/BarawikS/BarashPad/master/programms/rabbot.lua",{"internet", "chat_box"}},
---{{"UChatI"," - чат избранных (A: InfinityDark)"},"https://raw.githubusercontent.com/BarawikS/BarashPad/master/programms/UChat.lua",{"chat_box","internet","openperipheral_bridge"}},
+{{"logger"," - логгер чат"},"https://raw.githubusercontent.com/BarawikS/BarashPad/master/programms/Logger.lua",{"internet", "chat_box"}},
 {{"Antimute"," - обход мута в локале (A: InfinityDark)"},"https://raw.githubusercontent.com/BarawikS/BarashPad/master/programms/antimute.lua",{"chat_box"}}
