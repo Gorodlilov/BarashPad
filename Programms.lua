eb0f06becb196e338193b36890b05cd240901088
Kirill GorodiloV <45821562+BarawikS@users.noreply.github.com>
2019-08-20 16:03:34 +1000

    Update Programms.lua

--- Programms.lua
+++ Programms.lua
@@ -1,5 +1,5 @@
 {{"OpenBc"," - Объявления (A: Shyvana)"},"https://raw.githubusercontent.com/BarawikS/BarashPad/master/programms/OpenBC.lua",{"internet", "chat_box"}},
 {{"Holograms"," - голограммы"},"https://gorodlilov.ru/raw/holoedit.lua",{"internet", "hologram"}},
 {{"Board"," - инф. стенд"},"https://raw.githubusercontent.com/BarawikS/BarashPad/master/programms/board.lua",{"internet"}},
-{{"logger"," - логгер чат"},"https://raw.githubusercontent.com/BarawikS/BarashPad/master/programms/Logger.lua",{"internet", "chat_box"}},
+{{"logger"," - логгер чат"},"https://raw.githubusercontent.com/BarawikS/BarashPad/master/programms/logs/Logger.lua",{"internet", "chat_box"}},
 {{"Antimute"," - обход мута в локале (A: InfinityDark)"},"https://raw.githubusercontent.com/BarawikS/BarashPad/master/programms/antimute.lua",{"chat_box"}}
